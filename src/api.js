be7b4ac72c5464d8236fe5b687baba1a19f23696
HiroshiOyamada <h5y1m141@gmail.com>
2016-04-04 18:57:30 +0900

    initial commit

---
+++ src/api.js
@@ -0,0 +1,15 @@
+// var WebAPI = (function() {
+//   function WebAPI(){
+//     this.url = 'https://gist.githubusercontent.com/h5y1m141/f52eece296999105742c/raw/f3291233f69032a0b168192e11958575836833c2/react.json';
+//   }
+//   WebAPI.prototype.fetch = function(){
+//     var deferred;
+//     deferred = $.ajax({
+//       url: this.url
+//     });
+//     return deferred.promise();
+//   };
+//   return WebAPI;
+// }());
+
+console.log('web api');
